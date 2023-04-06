0f261af3a89c499abd68dfdc8b698d436232f057
TomiEtchabe33 <etchabetomas33@gmail.com>
2023-04-06 13:20:08 -0300

    contadorletraspalabras

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { TextField } from '@mui/material';
 
-function App() {
+const ContadorLetrasPalabras = () => {
+  const [texto, setTexto] = useState('');
+  const [contadorLetras, setContadorLetras] = useState(0);
+  const [contadorPalabras, setContadorPalabras] = useState(0);
+
+  useEffect(() => {
+    const palabras = texto.split(' ');
+    setContadorLetras(texto.length);
+    setContadorPalabras(palabras.filter(palabra => palabra !== '').length);
+    if (texto.length > 100) {
+      alert('Llegas al limite de 100 letras!');
+    }
+  }, [texto]);
+
+  const handleChangeTexto = (e) => {
+    setTexto(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      <center>
+      <br></br>
+      <h1>Contador de Letras y Palabras</h1>
+      <TextField id="outlined-basic" label="Escribir..." variant="outlined" value={texto} onChange={handleChangeTexto} />
+      <p className='contadores'>Contador de letras:<br></br> {contadorLetras}</p>
+      <p className='contadores'>Contador de palabras:<br></br> {contadorPalabras}</p>
+      </center>
     </div>
   );
-}
+};
 
-export default App;
+export default ContadorLetrasPalabras;
